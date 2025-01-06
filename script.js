a4cc758bd66472912d7350db6933bcfd4fad0015
Jake <114749332+somerandomguyonhere@users.noreply.github.com>
2025-01-06 13:02:46 -0500

    Update script.js
    stores counter using cookie

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-let count = 0; 
+let count = 0;
 
 const counterElement = document.getElementById('counter');
 const imageElement = document.getElementById('rockclicking');
 
 imageElement.addEventListener('click', () => {
-    count++; 
-    counterElement.textContent = count; 
+    count++;
+    counterElement.textContent = count;
+    document.cookie = "clickCount=" + count + "; expires=Thu, 31 Dec 9999 23:59:59 UTC; path=/";
 });
+
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+// get counter from cookie
+window.onload = function() {
+    let storedCount = getCookie("clickCount");
+    if (storedCount) {
+        count = parseInt(storedCount);
+        counterElement.textContent = count;
+    }
+}
